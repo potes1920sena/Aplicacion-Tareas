63fde4416ccd6e9ba57e55260ee1e6fc7593c57a
Oscar Eduardo Potes <38663780+potes1920sena@users.noreply.github.com>
2020-05-15 21:16:29 -0500

    Update database.php

--- conexion/database.php
+++ conexion/database.php
@@ -1,9 +1,9 @@
 <?php
 
 $connection = mysqli_connect(
-    'localhost',
-    'root',
-    '',
-    'app-tareas'
+     'mysqli.000webhost.com',
+    'id13720419_oscar',
+    '?VR2>2y>B@40[6{/',
+    'id13720419_app_tareas'
 );
 ?>
